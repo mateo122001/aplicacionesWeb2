508445352c6c09ea3e7dcfd57e93dc2c0c51b1bc
mateo122001 <94429326+mateo122001@users.noreply.github.com>
2022-10-13 22:14:45 -0500

    Update crudAprendizaje.js

--- Primer Parcial/practicaComplementaria-3/crudAprendizaje.js
+++ Primer Parcial/practicaComplementaria-3/crudAprendizaje.js
@@ -7,6 +7,7 @@ const mongoose=require("mongoose")
 const urlConnection="mongodb+srv://mateoparraga17:gostosaArlene@cluster0.6minr6o.mongodb.net/test";
 
 async function crudAprendizaje(){
+    try{
     const establishConnection=await mongoose.connect(urlConnection)
 
     const Aprendizaje=mongoose.model("Aprendizaje", {idIdioma:{type:mongoose.Types.ObjectId, ref:"Idioma"}, 
@@ -20,6 +21,9 @@ async function crudAprendizaje(){
     const results=await Aprendizaje.find()
 
     console.log(`Los elementos disponibles en esta entidad o modelo son: ${results}`)
+    }catch(error){
+        console.log(error)
+    }
 }
 
 crudAprendizaje()
