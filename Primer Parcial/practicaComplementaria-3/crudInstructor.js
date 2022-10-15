01f78df0a91fcd3ee577f699659aecd6d2bcfb2c
mateo122001 <94429326+mateo122001@users.noreply.github.com>
2022-10-14 21:32:12 -0500

    Update crudInstructor.js

--- Primer Parcial/practicaComplementaria-3/crudInstructor.js
+++ Primer Parcial/practicaComplementaria-3/crudInstructor.js
@@ -1,5 +1,4 @@
 
-
 const mongoose=require("mongoose")
 
 const urlConnection="mongodb+srv://mateoparraga17:gostosaArlene@cluster0.6minr6o.mongodb.net/test";
@@ -14,15 +13,30 @@ async function crudInstructor(){
 const establishConnection=await mongoose.connect(urlConnection)
 
 
-const instructor1=new Instructor({nombre:"Cristiano Ronaldo"})
-
+const instructor1=new Instructor({nombre:"Juan Delgado", experiencia:"5 años"})
 const SaveInstructor1=await instructor1.save()
 
-console.log(`Se guardo de manera exitosa el instructor: ${SaveInstructor1}`)
+const instructor2=new Instructor({nombre:"Marcos Mera", experiencia:"3 años"})
+const SaveInstructor2=await instructor2.save()
+
+const instructor3=new Instructor({nombre:"Ivan Maldonado", experiencia:"3 años"})
+const SaveInstructor3=await instructor3.save()
+
+const instructor4=new Instructor({nombre:"Joselin Cedeño", experiencia:"4 años"})
+const SaveInstructor4=await instructor4.save()
+
+const instructor5=new Instructor({nombre:"Maria Romero", experiencia:"3 años"})
+const SaveInstructor5=await instructor5.save()
+
 
 const results=await Instructor.find()
 
-console.log(`Los elementos disponibles en esta entidad o modelo son: ${results}`)
+
+console.log(`Los elementos disponibles en esta entidad o modelo son: `)
+
+results.forEach((elemento)=>{
+    console.log(elemento)
+})
 
 }catch(error)
 {
@@ -30,6 +44,6 @@ console.log(`Los elementos disponibles en esta entidad o modelo son: ${results}`
 }
 
 }
-module.exports=Instructor
+
 crudInstructor()
 
